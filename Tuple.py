9251f1db760a4342d73fbc3b474245ff7d05e5da
Marshall-IronSide <meliodas.martin@gmail.com>
2025-03-18 14:42:05 +0000

    Last python class

--- Tuple.py
+++ Tuple.py
@@ -1,1 +1,12 @@
 #Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+mytuple = ("apple", "banana", "cherry")
+print(mytuple)
+#Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created
+print(len(mytuple))
+
+#To create a tuple with only one item, you have to add a comma after the item
+thistuple = ("applle",)
+print(type(thistuple))
+
+thistuple = ("applle")#reconized as a string
+print(type(thistuple))
